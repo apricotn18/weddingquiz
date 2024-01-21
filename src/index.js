3a64ccdc948709a9ac401d2c4917df527a0e6094
kyoko nozaki <nozaki_kyoko@waku-2.com>
2024-01-21 22:26:01 +0900

    complete

--- src/index.js
+++ src/index.js
@@ -1,47 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./reset.scss";
 import "./style.scss";
-import image1 from "./img/1.jpg";
-import image2 from "./img/2.jpg";
-import image4 from "./img/4.jpg";
-import image5 from "./img/5.jpg";
-import image7 from "./img/7.jpg";
-import image8 from "./img/8.jpg";
 import imageNoren from "./img/noren.png";
 import imageTitle from "./img/quiztitle.png";
 import imageA from "./img/A.png";
 import imageB from "./img/B.png";
 import imageC from "./img/C.png";
 import imageD from "./img/D.png";
-
-const list = [
-	{
-		image: image1,
-		text: "【表参道駅 A2出口】から地上に出ます。",
-	},
-	{
-		image: image2,
-		text: "地上へ上がると、すぐ右手にApple Storeがあります。真っすぐ進み、Apple Storeの角を右に曲がってください。",
-	},
-	{
-		image: image4,
-		text: "約100m先の左手にflying tigerがあります。その角を左に曲がってください。",
-	},
-	{
-		image: image5,
-		text: "曲がるとすぐ左手にAfternoon Teaがあります。Afternoon Teaの前にあるT字路を右に曲がってください。",
-	},
-	{
-		image: image7,
-		text: "約100m先の右手に杉養蜂園があります。その角を右に曲がってください。",
-	},
-	{
-		image: image8,
-		text: "左手に「CHARMANT SCENA」という看板が見えます。到着です！！",
-	}
-];
+import imageFlower from "./img/flower.png";
+import imageHint2 from "./img/hint2.jpg";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -50,31 +18,6 @@ root.render(
 	</>
 );
 
-function Home () {
-	return (
-		<>
-			<div className="map">
-				<a href="https://maps.app.goo.gl/4UDZJjF2KyMrZHZM9">
-					<span>Google Mapはこちら</span>
-				</a>
-			</div>
-			<div className="wrapper">
-				<header>
-					<h1><span>笠原家・野崎家</span><br />シャルマンシーナ東京 道案内</h1>
-				</header>
-				<ul>
-					{list.map((item, index) =>
-					<li key={index}>
-						<div><img src={item.image} width="540" alt="道案内" /></div>
-						<p>{item.text}</p>
-					</li>
-					)}
-				</ul>
-			</div>
-		</>
-	);
-}
-
 function Quiz () {
 	return (
 		<>
@@ -82,10 +25,14 @@ function Quiz () {
 				<header className="quizHeader">
 					<img src={imageNoren} className="noren" width="540" alt="幼少期当てクイズ" />
 					<h1 className="title"><img src={imageTitle} width="540" alt="幼少期当てクイズ" /></h1>
-					<div className="textWrapper">
-						<div className="text">新郎新婦がペアになっている幼少期の写真はどれでしょう？<br /><span>受付エリアにある投票箱へ投票してください</span></div>
-					</div>
 				</header>
+				<div className="textWrapper">
+					<div className="text">
+						新郎新婦がペアになっている幼少期の写真はどれでしょう？
+						<span>受付エリアにある投票箱へ投票してください</span>
+						<a href="#hint">ヒントはこちら</a>
+					</div>
+				</div>
 				<ul>
 					<li className="A">
 						<div><img src={imageA} width="540" alt="" /></div>
@@ -100,8 +47,19 @@ function Quiz () {
 						<div><img src={imageD} width="540" alt="" /></div>
 					</li>
 				</ul>
-				<div className="hint">ヒント：新郎には兄、新婦には弟妹がいます</div>
+				<div id="hint" className="hint1">
+					<h2><img src={imageFlower} className="flower" width="16" alt="" />ヒント1<img src={imageFlower} className="flower" width="16" alt="" /></h2>
+					<p>新郎には兄、新婦には弟妹がいます</p>
+				</div>
+				<div className="hint2">
+					<h2><img src={imageFlower} className="flower" width="16" alt="" />ヒント2<img src={imageFlower} className="flower" width="16" alt="" /></h2>
+					<p>現在の新郎新婦の写真</p>
+					<div className="hintimagewrapper"><img src={imageHint2} className="hintimage" width="16" alt="" /></div>
+				</div>
 			</div>
+			<footer>
+				<small>ささやかですが景品を用意しています<br />ぜひご参加ください！</small>
+			</footer>
 		</>
 	);
 }
